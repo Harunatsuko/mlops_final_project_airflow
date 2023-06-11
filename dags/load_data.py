fd83e19657314b60dce5306f35bf15df8b1271f6
Catherine <harunatsurin@gmail.com>
2023-06-12 00:43:29 +0500

    changed load data to get iam_token

--- dags/load_data.py
+++ dags/load_data.py
@@ -1,4 +1,5 @@
 import logging
+import jwt
 from datetime import datetime
 import requests
 
@@ -19,10 +20,41 @@ dag = DAG(
     tags=['final_project'],
 )
 
+def gen_token():
+    service_account_id = Variable.get('SERV_ACC_ID')
+    key_id = Variable.get('SERV_ACC_PUB_KEY_ID')
+    secret_filepath = Variable.get('SECRET_FILEPATH')
+
+    with open(secret_filepath, 'r') as private:
+        private_key = json.load(private) # Чтение закрытого ключа из файла.
+        private_key = private_key['private_key']
+
+    now = int(time.time())
+    payload = {
+            'aud': 'https://iam.api.cloud.yandex.net/iam/v1/tokens',
+            'iss': service_account_id,
+            'iat': now,
+            'exp': now + 360}
+
+    encoded_token = jwt.encode(
+        payload,
+        private_key,
+        algorithm='PS256',
+        headers={'kid': key_id})
+
+    return encoded_token
+
 def get_vm_ip_address():
     INSTANCE_ID = Variable.get('INSTANCE_ID')
+
+    wt_token = gen_token()
+    IAM_TOKEN = requests.post(iam_token_url,
+                            json={"jwt":jwt_token.decode('ascii')},
+                            headers = {'Content-Type':'application/json'})
+    iam_token = IAM_TOKEN.json()['iamToken']
+
     url = 'https://compute.api.cloud.yandex.net/compute/v1/instances/{}'.format(INSTANCE_ID)
-    res = requests.get(url)
+    res = requests.get(url, headers = {'Authorization': 'Bearer {}'.format(iam_token)})
     print(res)
     ip_addr = res['networkInterfaces']['primaryV4Address']['address']
     return ip_addr
